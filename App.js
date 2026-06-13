d91103d8791d006e660489a2a228d43c5d9d7676
jonasmesk278 <jonasmesquita904@gmail.com>
2026-06-13 17:59:34 -0300

    Update App.js

--- App.js
+++ App.js
@@ -9,8 +9,67 @@ import * as Sharing from 'expo-sharing';
 import * as DocumentPicker from 'expo-document-picker';
 import * as FileSystem from 'expo-file-system';
 
-// Estrutura completa baseada na sua planilha de produção
-import { DICIONARIO_PRODUCAO } from './estruturaDados'; 
+// Estrutura completa declarada diretamente no aplicativo
+const DICIONARIO_PRODUCAO = {
+  "Crianças (< 1 ano)": [
+    "Nascidos vivos no mês", "1ª Consulta até o 30º dia de vida", "RN pesados ao nascer, com peso < 2500g",
+    "Visitas domiciliares antes de 7 dias após o nascimento", "Crianças de 0 a 6 meses", "2 visitas do ACS até 6 meses",
+    "Aleitamento exclusivo", "Menores de 1 ano", "Com vacinas em dia", "Pesadas", "Baixo peso", "Baixo Risco",
+    "Medio Risco", "Alto Risco"
+  ],
+  "Crianças (Acompanhamento)": [
+    "Menores de 2 anos", "9 consultas até 2 anos de vida", "Com vacinas em dia", "9 registros antropométricos", 
+    "Pesadas", "Baixo peso", "Baixo Risco", "Medio Risco", "Alto risco", "Acompanhadas pelo CEAMI",
+    "Crianças até 5 anos", "Menores de 6 anos", "Com vacinas em dias", "Baixo peso", "Baixo Risco", "Médio Risco", "Alto Risco"
+  ],
+  "Gestantes": [
+    "Nº de Gestantes cadastradas no mês", "Total de Gestantes Cadastradas na ESF", "< 16 anos cadastradas",
+    "Com pré-natal iniciado até 12ªsem", "Com 7 consultas de pré-natal", "Consulta Odontológica",
+    "Com citologia realizada", "Com vacinas em dia", "Com Teste de HIV e Sífilis 1º Trimestre",
+    "Com Teste de HIV e Sífilis 3º Trimestre", "Com 7 registros de PA", "Com 7 registros antropométricos",
+    "3 visitas do ACS", "Risco Habitual", "Risco Habitual de Maior vigilância", "Alto risco", "Acompanhadas no CEAMI"
+  ],
+  "Puerpéra": [
+    "Com 1 visitas do ACS (Até 42 dias)", "Com 1 Consulta Puerperal (Até 42 dias)"
+  ],
+  "Crônicos (Diabéticos)": [
+    "Diabéticos cadastrados", "Diabéticos visitados", "1 Consulta nos últimos 6 meses", "1 aferição de PA nos últimos 6 meses",
+    "1 registro antropométrico nos últimos 12 meses", "2 visitas do ACS nos últimos 12 meses",
+    "1 registro de hemoglobina glicada nos últimos 12 meses", "1 registro de avaliação de pé diabeticos nos últimos 12 meses",
+    "Baixo risco", "Médio Risco", "Alto risco", "Muito Alto Risco", "Insulinos Dependentes cadastrados"
+  ],
+  "Crônicos (Hipertensos)": [
+    "Hipertensos cadastrados", "Hipertensos acompanhados", "1 Consulta nos últimos 6 meses", "1 aferição de PA nos últimos 6 meses",
+    "1 registro antropométrico nos últimos 12 meses", "2 visitas do ACS nos últimos 12 meses", "Baixo risco",
+    "Médio Risco", "Alto risco", "Muito Alto Risco", "HAS aferição de pressão arterial (PA)"
+  ],
+  "Dados de Produção Crônica": [
+    "Pessoas com Hanseníase cadastradas", "Pessoas com Hanseníase acompanhados", "Pessoas com Tuberculose cadastradas",
+    "Pessoas com Tuberculose acompanhadas", "Pessoas com Obesidade cadastradas", "Pessoas com Obesidade acompanhadas",
+    "Pessoas com MNS(transtornos mentais, neurologicos e por uso de álcool e outras drogas) cadastradas",
+    "Pessoas com MNS(transtornos mentais, neurologicos e por uso de álcool e outras drogas) acompanhadas",
+    "Escalonamento", "PNCSM Leve", "PNCSM Moderado", "PNCSM Grave", "Visual", "Física/Motora", "Auditiva",
+    "Domiciliados", "Acamados", "Pacientes elegíveis para Cuidados Paliativos", "Pacientes em Cuidados Paliativos"
+  ],
+  "Dados Demográficos": [
+    "Número de pessoas de 15 ou mais anos", "Número de mulheres de 10 a 59 anos", "Número de mulheres de 25 a 64 anos",
+    "Número de pessoas na área cadastradas", "Número de famílias na área cadastradas", "Número de famílias na área não cadastradas",
+    "Famílias de Risco Habitual", "Família Risco 1", "Família Risco 2", "Família Risco 3", "Total de Educação em Saúde/ Atividade Coletiva"
+  ],
+  "Mulher & Idosos": [
+    "Nº de Citologia em mulheres entre 25 a 64 anos no mês", "14 a 69 anos com atendimento de saúde sexual e reprodutiva (12 meses)",
+    "Nº de Mamografia em mulheres entre 50 a 69 anos no mês", "Número de pessoas a partir de 60 anos", 
+    "1 Consulta nos últimos 12 meses", "Realização da Ficha de Avaliação do IVCF-20", "Idosos ROBUSTO", 
+    "Idosos POTENCIALMENTE FRÁGIL", "Idosos FRÁGIL", "Vacina influenza nos últimos 12 meses",
+    "1 Registro antropométrico nos últimos 12 meses", "2 visitas do ACS nos últimos 12 meses"
+  ],
+  "Adolescentes & Óbitos": [
+    "09 a 14 anos", "Com vacina HPV em dias", "11 a 12 anos", "Com vacina meningo ACWY em dias",
+    "De 0 a 6 dias", "De 7 a 28 dias", "De 29 dias a 11 meses e 29 dias", "Fetal", "De menor de 1 ano",
+    "De mulheres de 10 a 49 anos", "Maternos", "Domicílio ou via pública", "De adolescentes (10-19 anos) por violência",
+    "Outros óbitos", "Total de óbito", "Hospitalizações", "TOTAL DE VISITAS"
+  ]
+};
 
 export default function App() {
   const [mesAno, setMesAno] = useState(''); // Ex: "06/2026"
